e057f816e3c678082358f2be2d705babd3e0fea0
Mohit08-04-2006 <84503187+Mohit08-04-2006@users.noreply.github.com>
2021-07-27 15:01:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,110 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Body = Matter.Body;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground;
+var particle;
+
+var divisions = [];
+var plinkos = [];
+var divisionHeight = 200;
+
+var score = 0;
+var count = 0;
+
+var gameState = "PLAY";
+var END;
+
+function setup() {
+  var canvas = createCanvas(480,600);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  
+
+  ground = new Ground(240,570,width,10);
+
+  for (var k = 0; k <=width; k = k + 80) {
+    divisions.push(new Division(k,470, 10, divisionHeight));
+  }
+
+
+   for (var j = 30; j <=width; j=j+50) { 
+      plinkos.push(new Plinko(j,50));
+   }
+
+   for (var j = 60; j <=width-10; j=j+50) {
+      plinkos.push(new Plinko(j,100));
+   }
+
+    for (var j = 30; j <=width; j=j+50) {
+      plinkos.push(new Plinko(j,150));
+   }
+
+    for (var j = 60; j <=width-10; j=j+50) {
+      plinkos.push(new Plinko(j,200));
+   }
+
+   for (var j = 30; j <=width; j=j+50){
+     plinkos.push(new Plinko(j,250));
+   }
+   
+
+  canvas.mousePressed();
+  }
+
+ function mousePressed(){
+  particle = new Particles(mouseX,10,10);
+   console.log("Mohit"+mouseX);
+   //particle.display();
+
+  }
+
+
+function draw() {
+  background(0); 
+  textSize(20);
+  fill("skyblue");
+  text("score = "+score,10,20);
+  text("500",20,390);
+  text("500",100,390);
+  text("100",180,390);
+  text("100",260,390);
+  text("200",340,390);
+  text("200",420,390);
+
+  Engine.update(engine);
+
+  ground.display();
+  if (particle!=null){
+    particle.display();
+
+  if (particle.body.position.y>=400){
+    if(particle.body.position.x<=120){
+      score = score+500;
+     // particle = null;
+    }
+
+    if(particle.body.position.x>140 && particle.body.position.x<=240){
+      score = score+100;
+     // particle = null;  
+    }  
+
+    if(particle.body.position.x>260 && particle.body.position.x<=360){
+      score = score+200;
+     // particle = null;  
+    }  
+  }
+}
+
+  for (var i = 0; i < plinkos.length; i++) {
+    plinkos[i].display();
+  }
+
+  for (var k = 0; k < divisions.length; k++) {
+    divisions[k].display();
+  }
+}
